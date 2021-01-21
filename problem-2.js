0ca2b7f23022cfc1bffa0fd9324c45af19ebb504
jagadishchakma <jagadishchakma401@gmail.com>
2021-01-21 10:34:19 +0600

    reverse a string

---
+++ problem-2.js
@@ -0,0 +1,12 @@
+function reverse(speech){
+    var reverse = "";
+    for(var i = 0; i < speech.length; i++){
+        reverse = speech[i] + reverse; 
+    }
+
+    return reverse;
+}
+
+var speech = reverse('JavaScriopt is awesome programming language');
+
+console.log(speech);
