78d16f1e9828bcb7c132e25fbfc27445a7753c71
jagadishchakma <jagadishchakma401@gmail.com>
2021-01-21 11:32:26 +0600

    find the max value form multiple numbers

---
+++ problem-4.js
@@ -0,0 +1,13 @@
+// find the max value  of  multiple numbers 
+
+function max(numbers){
+    var result = 0;
+    for(var i = 0; i < numbers.length; i++){
+        if(result < numbers[i]){
+            result = numbers[i];
+        }
+    }
+    return result;
+}
+var numbers = [12,35,546,13245,342,300000];
+console.log(max(numbers));
